2ea13847178e75b615b57c785b69d9e95fcd34e8
haputhanthree <haputhanthree@gmail.com>
2020-06-28 14:04:29 +0530

    Updated

--- TelcoCustomerChurn.R
+++ TelcoCustomerChurn.R
@@ -1,3 +1,8 @@
+library(ade4)
+library(data.table)
+library(class)
+library(caret)
+
 #load data from CSV
 CustData = read.csv("Telco-Customer-Churn.csv" , header = TRUE)
 summary(CustData)
@@ -9,16 +14,15 @@ CustData = na.omit(CustData)
 
 #convert char into numeric
 #One-hot-encoding features:
-library(ade4)
-library(data.table)
-ohe_feats = c('gender', 'Partner', 'Dependents', 'PhoneService','MultipleLines','InternetService','OnlineSecurity'
+featureSet = c('gender', 'Partner', 'Dependents', 'PhoneService','MultipleLines','InternetService','OnlineSecurity'
               ,'OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies','Contract'
               ,'PaperlessBilling','PaymentMethod')
-for (f in ohe_feats){
+for (f in featureSet){
   df_all_dummy = acm.disjonctif(CustData[f])
   CustData[f] = NULL
   CustData = cbind(CustData, df_all_dummy)
 }
+
 #get structure of dataset
 str(CustData)
 
@@ -26,20 +30,17 @@ str(CustData)
 print(ncol(CustData))
 #remove cust ID and organize columns
 CustData = CustData[,c(2,3,4,5,7:47,6)]
-
+#get number of columns
 ncol(CustData)
 
-CustData[1:45]
 
-
-#Normalization
+#Normalization of the dataset
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x))) }
 
-CustDataNormalized <- as.data.frame(lapply(CustData[,1:44], normalize))
+CustDataNormalized = as.data.frame(lapply(CustData[,1:44], normalize))
 
-head(CustDataNormalized)
-
+#head(CustDataNormalized)
 
 set.seed(123)
 #random selection of 70% data.
@@ -57,45 +58,28 @@ test.CustData.labels  = CustData[-dat.d,46]
 #pCA ON train dataset
 train.CustData.pca = prcomp(train.CustData, center = TRUE, scale = TRUE)
 train.CustData.pca
-
+#summary of PCA results
 summary(train.CustData.pca)
 
 test.CustData.pca = predict(train.CustData.pca, newdata = test.CustData)
 
 
-
-
-
 #get number of rows in training dataset
 nrow(train.CustData)
-library(class)
 
+#test accuracy with K = 70 and 71
+knn.70 = knn(train=train.CustData.pca$x[,1:12], test=test.CustData.pca[,1:12], cl=train.CustData.labels, k=70)
+knn.71 = knn(train=train.CustData.pca$x[,1:12], test=test.CustData.pca[,1:12], cl=train.CustData.labels, k=71)
 
-#find optimum k value 
-i=1
-k.optm=1
-for (i in 1:12){
-  knn.mod = knn(train=train.CustData.pca$x[,1:12], test=test.CustData.pca[,1:12], cl=train.CustData.labels, k=i)
-  k.optm[i] = 100 * sum(test.CustData.labels == knn.mod)/NROW(test.CustData.labels)
-  k=i
-  cat(k,'=',k.optm[i],'
-    ')
-}
+#Calculate the proportion of correct classification for k = 70, 71
+ACC.70 = 100 * sum(test.CustData.labels == knn.70)/NROW(test.CustData.labels)
+ACC.71 = 100 * sum(test.CustData.labels == knn.71)/NROW(test.CustData.labels)
 
-# k =11 gives highest accuracy.
+#evaluate modle accuarcy using k =71 since it gives higher accuarcy 
+table(knn.70 ,test.CustData.labels)
 
-knn.11 = knn(train=train.CustData.pca$x[,1:12], test=test.CustData.pca[,1:12], cl=train.CustData.labels, k=11)
-#knn.71 = knn(train=train.CustData, test=test.CustData, cl=train.CustData.labels, k=71)
-
-#Calculate the proportion of correct classification for k = 26, 27
-ACC.11 = 100 * sum(test.CustData.labels == knn.11)/NROW(test.CustData.labels)
-#ACC.71 = 100 * sum(test.CustData.labels == knn.71)/NROW(test.CustData.labels)
-
-table(knn.11 ,test.CustData.labels)
-
-library(caret)
-
-confusionMatrix(table(knn.11 ,test.CustData.labels))
+#confution Matrix
+confusionMatrix(table(knn.70 ,test.CustData.labels))
 
 
 
